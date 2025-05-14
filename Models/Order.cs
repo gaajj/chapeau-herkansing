912dde99f113b4ce307cb3156a8d32f8c6ab009c
adam <adam8@live.nl>
2025-05-14 18:42:52 +0200

    dummy versie bar/kitchen
    ik heb order aangepast, een connection string toegevoegd, een controller aangemaakt, een view aangemaakt en main aangepast zodat de routing klopt en zodat alle css wordt weergeven.

--- Models/Order.cs
+++ Models/Order.cs
@@ -4,11 +4,11 @@ namespace ChapeauHerkansing.Models
     {
         public int OrderID { get; set; }
         public int? TableID { get; set; }
-        public string Feedback { get; set; }
+        public string? Feedback { get; set; }
         public bool? IsDeleted { get; set; }
 
         // Basic Constructor
-        public Order(int orderID, int? tableID, string feedback, bool? isDeleted)
+        public Order(int orderID, int? tableID, string? feedback, bool? isDeleted)
         {
             OrderID = orderID;
             TableID = tableID;
