b56d5e9fec646b32b9576adb02b5a1a2fcd460cb
adam <adam8@live.nl>
2025-06-16 19:15:00 +0200

    stock-bugfix

--- Repositories/Readers/OrderLineReader.cs
+++ Repositories/Readers/OrderLineReader.cs
@@ -13,7 +13,7 @@ namespace ChapeauHerkansing.Repositories.Readers
                 order,
                 MenuItemReader.Read(reader),
                 StaffReader.Read(reader),
-                reader.GetInt32(reader.GetOrdinal("stockAmount")),
+                reader.GetInt32(reader.GetOrdinal("amount")),
                 reader.GetDateTime(reader.GetOrdinal("orderLineOrderTime")),
                 reader.IsDBNull(reader.GetOrdinal("note")) ? null : reader.GetString(reader.GetOrdinal("note")),
                 ParseOrderStatus(reader.GetString(reader.GetOrdinal("orderStatus")))
